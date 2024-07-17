5d329f2c20fe5eb3184db3f221c430f1bb897d62
JuggerNutJoe <130806226+MinnieMia@users.noreply.github.com>
2024-07-17 18:31:02 -0500

    7f start there! U can do it! : )

--- MH-CyberFu/Assets/Scripts/Enemy controls.cs
+++ MH-CyberFu/Assets/Scripts/Enemy controls.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 5;
+    public float attackingDistance = 1;
+    public Vector3 direction;
+
+   private Animator animatorEnemy;
+   private Rigidbody rigidbodyEnemy;
+   private Transform target;
+   public bool isFollowingTarget;
+   private bool isAttackingTarget;
+   private float chasingPlayer = 0.01f;
+   public float currentAttackingTime;
+   public float maxAttackingTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFollowingTarget = true;
+        currentAttackingTime = maxAttackingTime;
     }
 
     // Update is called once per frame
